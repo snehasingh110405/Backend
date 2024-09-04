be07b6fa3cecd62090dacca0b50caf7f56c76450
snehasingh110405 <sne295463@gmail.com>
2024-09-04 23:28:52 +0530

    Update messageController.js

--- controller/messageController.js
+++ controller/messageController.js
@@ -3,7 +3,7 @@ export const sendMessage =async (req,res,next)=>{
     const {firstName,lastName,email,phone,message}=req.body;
     if(!firstName || !lastName || !email || !phone || !message){
         return res.status(400).json({
-            sucess: false,
+            success: false,
             message: "Please Fill Full Forms",
         });
     }    
